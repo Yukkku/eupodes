fe9604e0e0d05f6618366416626be6e84c6b2781
Yukkku <yukku.scratcher@gmail.com>
2023-08-21 09:32:23 +0900

    ここの更新忘れてた。ごめんね

--- data_structure/main.ts
+++ data_structure/main.ts
@@ -1,5 +1,5 @@
 export * from "./32ary_tree/main.ts";
-export * from "./avl_tree/main.ts";
+export * from "./avl/set.ts";
 export * from "./lagy_segment_tree/main.ts";
 export * from "./linked_list/main.ts";
 export * from "./potential_union_find/main.ts";
